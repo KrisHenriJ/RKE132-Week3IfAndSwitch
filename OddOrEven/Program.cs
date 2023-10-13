6537be398db80f3b0406058d0e1a45d531566379
Kris Henri Järvi <kris@Kriss-MacBook-Air.local>
2023-10-13 14:50:42 +0300

    Week3

---
+++ OddOrEven/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Enter a number:");
+int userNum = Convert.ToInt32(Console.ReadLine());
+
+//kas kasutaja arv on paaris või paaritu
+
+int result = userNumNum % 2;
+
+Console.WriteLine(result);
+
+if(result != 0) //!= - ei ole
+{
+    Console.WriteLine("User number is odd.");
+} else
+{
+    Console.WriteLine("User number is even.");
+}
